fc4a2a2f533e2e345bf8f1ae0227310fef23b22c
Marko Dobric <marko.dobric@activecollab.com>
2019-10-24 22:31:11 +0200

    Create day service

--- app/Service/DayService.php
+++ app/Service/DayService.php
@@ -1,10 +1,53 @@
 <?php
 
+declare(strict_type=1);
 
 namespace App\Service;
 
+use App\Aggregate\DayAggregate;
+use App\Repository\DayRepository;
+use App\Repository\MealRepository;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 
 class DayService
 {
+    /**
+     * @var DayRepository
+     */
+    private $dayRepository;
 
+    /**
+     * @var MealRepository
+     */
+    private $mealRepository;
+
+    public function __construct(
+        DayRepository $dayRepository,
+        MealRepository $mealRepository
+    )
+    {
+        $this->dayRepository = $dayRepository;
+        $this->mealRepository = $mealRepository;
+    }
+
+    public function getDaysCollection(
+        Carbon $startDate,
+        Carbon $endDate,
+        User $user
+    ): Collection
+    {
+        $collection = new Collection();
+
+        $days = $this->dayRepository->findByDateRangeAndUser($startDate, $endDate, $user);
+        foreach ($days as &$day) {
+            $day->meals = $this->mealRepository->findByDayAndUser($day, $user);
+            $collection->add(
+                (new DayAggregate($day))->map()
+            );
+        }
+
+        return $collection;
+    }
 }
