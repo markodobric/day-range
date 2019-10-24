f6ae7683db3ceb2bbfb74382cdca9cebc5735f2f
Marko Dobric <marko.dobric@activecollab.com>
2019-10-24 22:29:21 +0200

    Create repositories

--- app/Repository/DayRepository.php
+++ app/Repository/DayRepository.php
@@ -1,10 +1,20 @@
 <?php
 
-
 namespace App\Repository;
 
+use App\User;
+use Carbon\Carbon;
 
 class DayRepository
 {
-
+    public function findByDateRangeAndUser(Carbon $min, Carbon $max, User $user)
+    {
+        return Day::where([
+            ['date', '>=', $min->format('Y-m-d')],
+            ['date', '<=', $max->format('Y-m-d')],
+            ['user_id', '=', $user->id],
+        ])
+            ->orderBy('date', 'ASC')
+            ->get();
+    }
 }
--- app/Repository/MealRepository.php
+++ app/Repository/MealRepository.php
@@ -1,10 +1,15 @@
 <?php
 
-
 namespace App\Repository;
 
+use App\User;
 
 class MealRepository
 {
-
+    public function findByDayAndUser(Day $day, User $user)
+    {
+        return Meal::where('day_id', $day->id)
+            ->where('user_id', $user->id)
+            ->get();
+    }
 }
